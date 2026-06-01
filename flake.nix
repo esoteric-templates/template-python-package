4d75c47670b6a9c001584e0b004ac5661babdd24
Дима Ш. <dima.o.sh@proton.me>
2026-06-01 23:31:10 +0100

    Use 'legacyPackages.${system}' instead of import
    Apparently, this avoids duplicate nixpkgs:
    https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 		flake-utils.lib.eachDefaultSystem (system:
 		let
 			pname = "template";
-			pkgs = import nixpkgs { inherit system; };
+			pkgs = nixpkgs.legacyPackages.${system};
 		in {
 			packages.default = pkgs.python3Packages.buildPythonPackage {
 				inherit pname;
